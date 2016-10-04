db8a3506c495de6d6af438a60f356c8895fe3f76
Pierrick Vinot <pierrick.vinot@mastertp>
2016-10-04 12:06:41 +0200

    2e point de validation

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ var express = require("express");
 var app =express();
 
 var http = require("http");
+
 var CONFIG = require("./config.json");
 process.env.CONFIG = JSON.stringify(CONFIG);
 
-var defaultRoute = 	require("./app")
+var defaultRoute = 	require("./app/routes/default.route.js")
+
+app.use(defaultRoute);
 
 var server = http.createServer(app);
+console.log(CONFIG.port)
 server.listen(CONFIG.port);
 
